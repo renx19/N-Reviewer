33c00e9050ec5ba6e93fa9d1a05720ab614e6e79
renx19 <1markjustinborja1@gmail.com>
2023-10-25 11:13:43 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+     content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   darkMode: "class",
   mode: "jit",
   theme: {
